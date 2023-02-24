15035acb44183ca79cd8e1e018b400b5211b716c
GauravPandit-Webonise <gaurav.pandit@weboniselab.com>
2023-02-24 17:54:17 +0530

    feature | ruby string one

---
+++ assignment5b.rb
@@ -0,0 +1,4 @@
+def position(text)
+  puts text.index("RUBY")
+end
+position("RUBY parses a file by looking for <br/> one of the special tags that tells it to start interpreting the text as RUBY code. The parser then executes all of the code it finds until it runs into a RUBY closing <br/> tag.")
