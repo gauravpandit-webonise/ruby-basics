d589a124f27631c1c6cab88525a1f236c1b98a8d
GauravPandit-Webonise <gaurav.pandit@weboniselab.com>
2023-02-24 18:15:16 +0530

    feature | fing length of string

---
+++ assignment5j.rb
@@ -0,0 +1,6 @@
+def string_length(text1, text2)
+  puts text1.length
+  puts text2.length
+end
+
+string_length("RUBY parses a file by looking for <br/> one of the special tags that tells it to start interpreting the text as RUBY code. The parser then executes all of the code it finds until it runs into a RUBY closing <br/> tag.", "RUBY does not require (or support) explicit type definition in variable declaration; a variable's type is determined by the context in which the variable is used.")
